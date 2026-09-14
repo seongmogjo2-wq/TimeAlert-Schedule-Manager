d23e7c809f68502929507a1db1896ab2ff861471
seongmogjo2-wq <seongmogjo2@gmail.com>
2026-09-14 13:31:12 +0900

    fix:테스트 오류 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,17 +15,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const AlamApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
     expect(find.text('일정 관리'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
