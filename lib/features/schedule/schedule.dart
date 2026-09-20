85d8ae7b4cc919befd595ac6c80c8734f34aa59a
seongmogjo2-wq <seongmogjo2@gmail.com>
2026-09-20 09:47:05 +0900

    feat/#9: AsyncNotifierProvider 기반 비동기 일정 목록 구현

---
+++ lib/features/schedule/schedule.dart
@@ -0,0 +1,11 @@
+class Schedule {
+  final String title;
+  final String time;
+  final bool isAlarmOn;
+
+  const Schedule({
+    required this.title,
+    required this.time,
+    required this.isAlarmOn,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:timealert_schedule_manager/core/theme/app_theme.dart';
 import 'package:timealert_schedule_manager/screens/add_schedule_screen.dart';
@@ -6,9 +7,15 @@ import 'package:timealert_schedule_manager/screens/alam_screen.dart';
 import 'package:timealert_schedule_manager/screens/login_screen.dart';
 import 'package:timealert_schedule_manager/screens/set_screen.dart';
 
-void main() => runApp(AlamApp());
+void main() {
+  runApp(
+    const ProviderScope(
+      child: AlamApp(),
+    ),
+  );
+}
 
-class AlamApp extends StatelessWidget{
+class AlamApp extends StatelessWidget {
   const AlamApp({super.key});
 
   @override
@@ -24,9 +31,9 @@ class AlamApp extends StatelessWidget{
 final GoRouter router = GoRouter(
   initialLocation: '/login',
   routes: [
-    GoRoute(path: '/login', builder: (context, state) => LoginScreen()),
-    GoRoute(path: '/alam', builder: (context, state) => AlamScreen()),
-    GoRoute(path: '/set', builder: (context, state) => SetScreen()),
-    GoRoute(path: '/add', builder: (context,state) => AddScheduleScreen())
-  ]
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+    GoRoute(path: '/alam', builder: (context, state) => const AlamScreen()),
+    GoRoute(path: '/set', builder: (context, state) => const SetScreen()),
+    GoRoute(path: '/add', builder: (context, state) => const AddScheduleScreen()),
+  ],
 );
